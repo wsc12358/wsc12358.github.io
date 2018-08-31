722b7a4989e7632320f408c253bf642284e12421
wsc <wsc610322199406081138@outlook.com>
2018-08-31 20:14:48 +0800

    update

--- create.sh
+++ create.sh
@@ -16,7 +16,7 @@ FILE_NAME="$DATE-`echo $TITLE|sed 's/[ ][ ]*/-/g'`.md"
 echo "file name:" _posts/$FILE_NAME
 
 # template content
-CONTENT=`cat $TEMPLATE`
+#CONTENT=`cat $TEMPLATE`
 
 # fill title
 POST_TITLE=$TITLE
@@ -30,9 +30,25 @@ CONTENT=`echo "${CONTENT}" | sed "s/{time}/${DATE} ${TIME}/g"`
 
 # output file (check exists)
 if [ ! -e "$DIR/_posts/$FILE_NAME" ]; then
-    echo "${CONTENT}" > _posts/$FILE_NAME
+    echo "---" >>_posts/$FILE_NAME
+    echo "layout: post">>_posts/$FILE_NAME
+    echo "title: \"${POST_TITLE}\"">>_posts/$FILE_NAME
+    echo "background: red">>_posts/$FILE_NAME
+    echo "background-image:">>_posts/$FILE_NAME
+    echo "categories:">>_posts/$FILE_NAME
+    echo "-">>_posts/$FILE_NAME
+    echo "tags:">>_posts/$FILE_NAME
+    echo "-">>_posts/$FILE_NAME
+    echo "author: Dimension">>_posts/$FILE_NAME
+    echo "description:">>_posts/$FILE_NAME
+    echo "mermaid: true">>_posts/$FILE_NAME
+    echo "---">>_posts/$FILE_NAME
+    echo "\n">>_posts/$FILE_NAME
+    echo "* 目录   ">>_posts/$FILE_NAME
+    echo "{:toc #markdown-toc}">>_posts/$FILE_NAME
 else
     echo "file exists..." 
 fi
 
 echo "Successfully Create a markdown file in _post/"
+
